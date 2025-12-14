5ce5a087105a9bccc993815b88bd965a2f5b2c49
Fraol <fraolm41@gmail.com>
2025-12-14 23:40:53 +0300

    added mysql server

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='your_username',
+        password='your_password'
+    )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
